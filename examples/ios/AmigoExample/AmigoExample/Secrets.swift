0da927fbd479ff2b7d88bb4a10a250bfdfa46aed
Dinglun Fu <fudinglun@gmail.com>
2026-04-13 14:33:43 +0800

    add api key with new sdk

---
+++ examples/ios/AmigoExample/AmigoExample/Secrets.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+enum Secrets {
+    // TODO: Replace with your API key from https://sdk.amigoai.io/dashboard
+    // Never Commit Your API key if you are using it on a public repo
+    static let amigoAPIKey = "your-api-key"
+}
